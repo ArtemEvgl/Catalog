7586db3ea963b5877d148c20b8856aa7fce67afb
Artem Evgl <44645002+ArtemEvgl@users.noreply.github.com>
2022-01-23 15:53:07 +0300

    add authentication MongoDb

--- obj/Debug/net5.0/Catalog.AssemblyInfo.cs
+++ obj/Debug/net5.0/Catalog.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("060ddaea-1b17-46e9-8d57-6e69f2323b0c")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
